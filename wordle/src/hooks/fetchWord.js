9312b0f815489a0ad7ec19608822da23be1005c1
Benjamin-Meyer-Dev <benjamin.meyer.cloud@gmail.com>
2025-03-31 20:12:32 -0400

    Removing console test log.

--- wordle/src/hooks/fetchWord.js
+++ wordle/src/hooks/fetchWord.js
@@ -6,15 +6,11 @@ function FetchWord() {
     useEffect(() => {
         async function getWord() {
             const response = await fetch("https://random-word-api.herokuapp.com/word?length=5")
-
-            if (!response.ok)
-                throw new Error("Failed to fetch word.")
-
             const result = await response.json()
+
             setWord(result[0])
         }
 
-        console.log("fetching word...")
         getWord()
     }, [])
 
